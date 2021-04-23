79594eddb09dbfe40feef96e8c5c546c0452ffd9
glcoder0 <glcoder@mail.com>
2021-04-23 20:40:09 +0100

    added example for how to make eigen do MatrixElement*VectorElement->ProductType, AccumulatorOuputElement+=ProductType

--- cpp/eigen_experiment.cpp
+++ cpp/eigen_experiment.cpp
@@ -3,14 +3,18 @@
 
 // setup for MatElem*VecElem->Prod
 // goal is to add Prod+Prod->Acc,  Acc+=Prod,  Acc(Prod) Acc=0
+// dummy types for each part of the process we want...
 class MatElem {public:MatElem(){} MatElem(int x){}};
 class VecElem {public:VecElem(){} VecElem(int x){}};
 class Prod {public:Prod(){} Prod(int x){}};
 class Acc {public:Acc(){} Acc(const Prod&){printf("Acc(Prod)\n");} Acc(Prod&){printf("Acc(Prod)\n");} Acc(int x){}};
+// operators to combine information betwee matrix elems, input vector elems, and the output Accumulator, "Acc"
 auto operator*(const MatElem& a,const VecElem& b){printf("MatElem*VecElem\n");return Prod{};}
 auto operator+(const Prod& a,const Prod& b){printf("Prod+Prod\n");return Prod{};}
 auto operator+=(Prod& a,const Prod& b){printf("Prod+=Prod\n");return a;}
 auto operator+=(Acc& a,const Prod& b){printf("Acc+=Prod\n");return a;}
+
+// Type Traits needed for Eigens' templates to work. needed for MatElem,VecElem,Prod,Acc
 template<>
 class Eigen::NumTraits<MatElem> {
 public:
@@ -23,6 +27,8 @@ public:
 		IsSigned=1,
 		RequireInitialization=1,
 		IsComplex=0,
+		// eigens templates apparently use these estimated costs 
+ 		// to decide when to combine expressions (wow)
 		ReadCost=1,
 		AddCost=1,
 		MulCost=1
@@ -106,9 +112,15 @@ public:
 	auto static digist10(){return 5;}
 };
 
+// "operator traits" tell Eigen what types to create for its outputs . 
+// (it can't just use decltype(inputs*..) ??)
 template<>
 struct Eigen::ScalarBinaryOpTraits<MatElem,VecElem,Eigen::internal::scalar_product_op<MatElem, VecElem> >{
-	typedef Acc ReturnType;
+	// MatElem*VecElem returns Product in our overload, but
+	// telling it "Acc" here makes it sum and store those products into this Accumulator.
+	// the mismatch sounds wrong but is actually what lets us have "a trivial type for the product"
+	// and potential "nontrivial type for the accumulator (eg spike queue)
+	typedef Acc ReturnType; 
 };
 
 template<>
@@ -130,15 +142,18 @@ void eigen_experiment() {
 	myvec.insert(1)=VecElem{};
 	myvec.insert(2)=VecElem{};
 
-	Eigen::SparseVector<Acc> tmp=mymat*myvec;
+	// run the matrix multiply, the prints in the operators will notify us when it calls each operator
+	Eigen::SparseVector<Acc> result=mymat*myvec;
 	
+	// debug print the matrix filled indices
 	for (int k=0; k<mymat.outerSize(); ++k){
 		for (decltype(mymat)::InnerIterator v(mymat,k); v;++v){
 			printf("%d %d\n",v.row(),v.col());
 		}
 	}
 
-	for (decltype(tmp)::InnerIterator v(tmp); v;++v){
+	// debug print the output vector filled indices
+	for (decltype(result)::InnerIterator v(result); v;++v){
 		printf("%d\n",v.index());
 	}
 }
